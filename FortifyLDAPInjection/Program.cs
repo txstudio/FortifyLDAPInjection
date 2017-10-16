e897163dfea58f99bbf4ed537ecc6c55810055b9
Shi Hao Wang <st890409@hotmail.com>
2017-10-16 09:29:23 +0800

    LDAP Injection 範例程式碼

--- FortifyLDAPInjection/Program.cs
+++ FortifyLDAPInjection/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.Security.Application;
+using System;
+using System.DirectoryServices;
 
 namespace FortifyLDAPInjection
 {
     class Program
     {
+        const string entryPath = "LDAP://<domain>";
+
         static void Main(string[] args)
         {
+            string _account;
+            string _filter;
+
+            DirectoryEntry _entry;
+            DirectorySearcher _searcher;
+
+            _entry = new DirectoryEntry();
+            _entry.Path = entryPath;
+
+            _account = args[0];
+
+            _filter = "(&(objectCategory=user)(CN=" + _account + "))";
+            //_filter = Encoder.LdapFilterEncode(_filter);
+
+            _searcher = new DirectorySearcher();
+
+            _searcher.Filter = _filter;
+            _searcher.SearchScope = SearchScope.Subtree;
+            _searcher.SearchRoot = _entry;
+
+            var items = _searcher.FindAll();
+            var item = items[0];
+
+
+            Console.WriteLine(items.Count);
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
         }
     }
 }
